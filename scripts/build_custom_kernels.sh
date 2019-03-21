f2cd0d0a039d9d5bf368ba3eeb5b1a811d08f003
Karen Hambardzumyan <mail@mahnerak.com>
2019-03-21 18:35:40 +0400

    Make possible to build GPU kernel on non-GPU machine

--- scripts/build_custom_kernels.sh
+++ scripts/build_custom_kernels.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Build custom kernels.
-TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-g++ -std=c++11 -shared srl_kernels.cc -o srl_kernels.so -I$TF_INC -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -fPIC -D_GLIBCXX_USE_CXX11_ABI=0 -O2
+TF_ORIGIN=$(python -c 'import importlib; print(importlib.util.find_spec("tensorflow").origin)')
+TF_DIR=$(dirname $TF_ORIGIN)
+TF_INC=$TF_DIR/include
+TF_LIB=$TF_DIR
+
+g++ -std=c++11 -shared srl_kernels.cc -o srl_kernels.so \
+    -I$TF_INC -I$TF_INC/external/nsync/public \
+    -L$TF_LIB -ltensorflow_framework \
+    -fPIC -D_GLIBCXX_USE_CXX11_ABI=0 -O2
